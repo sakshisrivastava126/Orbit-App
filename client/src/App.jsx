a2837e7cc90516e8ccc864abcb47418308566022
Sakshi Srivastava <sakshisrivastava@Sakshis-MacBook-Air.local>
2026-01-18 01:16:33 +0530

    Fix background image for Vite production build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,20 @@ import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import { Toaster } from "react-hot-toast"
 import { AuthContext } from "./context/AuthContext.jsx";
-
+import bgImage from "./assets/bgImage2.png";
 
 const App = () => {
-  const { authUser, loading } = useContext(AuthContext)
+  const { authUser } = useContext(AuthContext)
 
   if (loading) {
     return <div className="text-black p-4 bg-gray-200">Loading...</div>;
   }
 
   return (
-    <div className="bg-[url('./src/assets/bgImage2.png')] bg-contain">
+    <div
+      style={{ backgroundImage: `url(${bgImage})` }}
+      className="bg-contain"
+    >
       <Toaster />
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
